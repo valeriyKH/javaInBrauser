e59866e4ce0fd3f4e6d7b6e449b7299ff082c304
valera91k@yandex.ru <valera91k@yandex.ru>
2022-06-25 22:33:05 +0300

    Added slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,34 +1,34 @@
-let prevBtn = document.querySelector(".slider__arrow_prev");
-let nextBtn = document.querySelector(".slider__arrow_next");
-let imgList = Array.from(document.querySelectorAll(".slider__item"));
-let dotList = Array.from(document.querySelectorAll(".slider__dot"));
+let sliderPrev = document.querySelector(".slider__arrow_prev");
+let nextSlider = document.querySelector(".slider__arrow_next");
+let images = Array.from(document.querySelectorAll(".slider__item"));
 
-let clearActiveClass = () => {
-  imgList.forEach((element) => element.classList.remove("slider__item_active"));
+
+let classDelete = () => {
+  images.forEach((element) => element.classList.remove("slider__item_active"));
 };
 
-let addActiveClass = (i) => {
-  imgList[i].classList.add("slider__item_active");
+let adderClass = (i) => {
+  images[i].classList.add("slider__item_active");
 };
 
-nextBtn.onclick = () => {
-  let i = imgList.findIndex((element) =>
+nextSlider.onclick = () => {
+  let i = images.findIndex((element) =>
     element.classList.contains("slider__item_active")
   );
-  if (i >= imgList.length - 1) {
+  if (i >= images.length - 1) {
     i = -1;
   }
-  clearActiveClass();
-  addActiveClass(i + 1);
+  classDelete();
+  adderClass(i + 1);
 };
 
-prevBtn.onclick = () => {
-  let i = imgList.findIndex((element) =>
+sliderPrev.onclick = () => {
+  let i = images.findIndex((element) =>
     element.classList.contains("slider__item_active")
   );
   if (i <= 0) {
-    i = imgList.length;
+    i = images.length;
   }
-  clearActiveClass();
-  addActiveClass(i - 1);
+  classDelete();
+  adderClass(i - 1);
 };
