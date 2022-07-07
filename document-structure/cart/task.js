0542770c1fd1c889a764388c8c6ec73d49e93ca3
valera91k@yandex.ru <valera91k@yandex.ru>
2022-07-07 23:38:38 +0300

    Add structure

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,20 +1,21 @@
-const cart = document.querySelector('.cart__products');
+const cart = document.querySelectorAll('.cart__products');
 
 const productsList = document.querySelectorAll('.product');
-let arr = [...productsList].map((item) => item.getAttribute('data-id'));
+let arr = [...productsList].map((item) => item.getAttribute('daata-id'));
 
 getInfoAboutProduct = (item) => {
     const controlsItem = item.querySelector('.product__controls');
     const countBlock = controlsItem.querySelector('.product__quantity-value');
-    const imgItem = item.querySelector('.product_image').src;
+    const imgItem = item.querySelector('.product__image').src;
     const quantity = item.querySelectorAll('.product__quantity-control');
-    const dataId = item.getAttribute('data-id');
+    const dataId = item.getAttribute('.data-id');
 
     quantity.forEach(item => {
         item.addEventListener('click', e => {
-            if(e.target.classList.contains('.product__quantity-control_dec')){
-                if(countBlock.textContent > 1) {
-                    countMlock.innerHTML = countBlock.textContent -1;
+
+            if(e.target.classList.contains('product__quantity-control_dec')) {
+                if (countBlock.textContent > 1) {
+                    countBlock.innerHTML = countBlock.textContent -1;
                 }
             } else {
                 countBlock.innerHTML = +countBlock.textContent + 1;
@@ -29,19 +30,23 @@ getInfoAboutProduct = (item) => {
         div.setAttribute('id', dataId);
         div.classList.add('cart__product');
 
-        div.innerHTML = ` 
-        <img class = 'cart__product-image' src=${imgItem}>
-        <div class = 'cart__product-count'>${countBlock.textContent}</div>
-        `;
-
+        div.innerHTML = `
+    <img class ='cart__product-image' src=${imgItem}>
+    <div class ='cart__product-count>${countBlock.textContent}</div>
+    `;
         const idRenderElem = arr.find(item => item === dataId);
 
         if(document.getElementById(idRenderElem)) {
             const foundElement = document.getElementById(idRenderElem);
             const prevCount = foundElement.querySelector('.cart__product-count').textContent;
-            foundElement.querySelector('.cart__product-count').innerHTML = countBlock.textContent + +prevCount;
+            foundElement.querySelector('.cart__product-count').innerHTML = +countBlock.textContent + +prevCount;
         } else {
+
             cart.appendChild(div);
         }
     });
 };
+
+productsList.forEach((el) => {
+    getInfoAboutProduct(el);
+});
